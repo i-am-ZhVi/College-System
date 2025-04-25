db2da226f65fb46d82d63b4b6691788f83246a0f
i-am-ZhVi <unius6ls66@gmail.com>
2025-04-25 16:28:46 +0300

    additional tables to the system model

--- backend/src/models/system.py
+++ backend/src/models/system.py
@@ -1,3 +1,5 @@
+import enum
+
 from typing import Annotated
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql.schema import ForeignKey
@@ -27,15 +29,6 @@ class Item(Base):
     post: Mapped[int] = mapped_column(ForeignKey("post.id", ondelete="CASCADE"))
 
 
-class Group(Base):
-    __tablename__ = "group"
-
-    id: Mapped[my_id]
-    name: Mapped[str]
-    professions: Mapped[bool] = mapped_column(default=False)
-    denominator: Mapped[bool] = mapped_column(default=False)
-
-
 class Grade(Base):
     __tablename__ = "grade"
 
@@ -46,3 +39,93 @@ class Grade(Base):
     truancy: Mapped[bool]
     date: Mapped[datetime]
     number_couple: Mapped[int]
+
+
+
+class Group(Base):
+    __tablename__ = "group"
+
+    id: Mapped[my_id]
+    name: Mapped[str]
+    professions: Mapped[bool] = mapped_column(default=False)
+    denominator: Mapped[bool] = mapped_column(default=False)
+
+
+class Student_to_Group(Base):
+    __tablename__ = "student_to_group"
+
+    person_id: Mapped[int] = mapped_column(ForeignKey("person.id", ondelete="CASCADE"), primary_key=True)
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"), primary_key=True)
+    is_elder: Mapped[bool] = mapped_column(default=False)
+
+
+class Teacher_to_Group(Base):
+    __tablename__ = "teacher_to_group"
+
+    person_id: Mapped[int] = mapped_column(ForeignKey("person.id", ondelete="CASCADE"), primary_key=True)
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"), primary_key=True)
+    is_master: Mapped[bool] = mapped_column(default=False)
+
+
+class Item_for_Group(Base):
+    __tablename__ = "item_for_group"
+
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"), primary_key=True)
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id", ondelete='CASCADE'), primary_key=True)
+
+
+class Days(enum.Enum):
+    mondey = "Понедельник"
+
+
+
+class Specialties(Base):
+    __tablename__ = "specialties"
+
+    id: Mapped[my_id]
+    weekly_day: Mapped[Days]
+    teacher_id: Mapped[int] = mapped_column(ForeignKey("teacher.id", ondelete="CASCADE"))
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id", ondelete="CASCADE"))
+    number_couple: Mapped[int]
+    distance: Mapped[bool] = mapped_column(default=False)
+    denominator: Mapped[bool] = mapped_column(default=False)
+
+
+class Professions(Base):
+    __tablename__ = "professions"
+
+    id: Mapped[my_id]
+    weekly_day: Mapped[Days]
+    teacher_id: Mapped[int] = mapped_column(ForeignKey("teacher.id", ondelete="CASCADE"))
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id", ondelete="CASCADE"))
+    number_couple: Mapped[int]
+    distance: Mapped[bool] = mapped_column(default=False)
+    denominator: Mapped[bool] = mapped_column(default=False)
+
+
+class Substitutions_Specialties(Base):
+    __tablename__ = "substitutions_specialties"
+
+    id: Mapped[int] = mapped_column(ForeignKey("specialties.id", ondelete="CASCADE"), primary_key=True)
+    weekly_day: Mapped[Days]
+    teacher_id: Mapped[int] = mapped_column(ForeignKey("teacher.id", ondelete="CASCADE"))
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id", ondelete="CASCADE"))
+    number_couple: Mapped[int]
+    distance: Mapped[bool] = mapped_column(default=False)
+    denominator: Mapped[bool] = mapped_column(default=False)
+
+
+class Substitutions_Professions(Base):
+    __tablename__ = "substitutions_professions"
+
+    id: Mapped[int] = mapped_column(ForeignKey("professions.id", ondelete="CASCADE"), primary_key=True)
+    weekly_day: Mapped[Days]
+    teacher_id: Mapped[int] = mapped_column(ForeignKey("teacher.id", ondelete="CASCADE"))
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id", ondelete="CASCADE"))
+    item_id: Mapped[int] = mapped_column(ForeignKey("item.id", ondelete="CASCADE"))
+    number_couple: Mapped[int]
+    distance: Mapped[bool] = mapped_column(default=False)
+    denominator: Mapped[bool] = mapped_column(default=False)
