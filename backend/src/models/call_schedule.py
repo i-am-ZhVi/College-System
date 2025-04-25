ef29182437adafb5ccbdf5bdabdf8bb255f0e971
i-am-ZhVi <unius6ls66@gmail.com>
2025-04-25 22:50:07 +0300

    additional tables to the call_shedule model

---
+++ backend/src/models/call_schedule.py
@@ -0,0 +1,33 @@
+
+from datetime import datetime
+from sqlalchemy.orm import mapped_column, Mapped
+from database import Base
+
+
+
+class Time_Couple(Base):
+    __tablename__ = "time_couple"
+
+    number_couple: Mapped[int] = mapped_column(primary_key=True)
+    start_time: Mapped[str]
+    end_time: Mapped[str]
+
+
+class Saturday_Time_Couple(Base):
+    __tablename__ = "saturday_time_couple"
+
+    number_couple: Mapped[int] = mapped_column(primary_key=True)
+    start_time: Mapped[str]
+    end_time: Mapped[str]
+
+
+class Change_Time_Couple(Base):
+    __tablename__ = "change_time_couple"
+
+    start_date: Mapped[datetime]
+    end_date: Mapped[datetime]
+    number_couple: Mapped[int] = mapped_column(primary_key=True)
+    start_time: Mapped[str]
+    end_time: Mapped[str]
+    saturday: Mapped[bool] = mapped_column(default=False)
+    all_days: Mapped[bool] = mapped_column(default=False)
