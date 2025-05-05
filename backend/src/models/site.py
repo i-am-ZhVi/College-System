70caf8af5449e220097152f2289d3bea60d0dfd0
i-am-ZhVi <unius6ls66@gmail.com>
2025-05-05 13:15:07 +0300

    create model for web site

---
+++ backend/src/models/site.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Annotated
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.orm.properties import ForeignKey
+from sqlalchemy.sql import expression
+from database import Base
+
+my_id = Annotated[int, mapped_column(primary_key=True)]
+
+class News_Page(Base):
+    __tablename__ = "news_page"
+
+    id: Mapped[my_id]
+    message: Mapped[str] = mapped_column(nullable=True)
+
+    files: Mapped[list["File"]] = relationship(
+        back_populates="pages",
+        secondary="file_to_main_page"
+    )
+
+
+class File_to_Main_Page(Base):
+    __tablename__ = "file_to_main_page"
+
+    news_id: Mapped[int] = mapped_column(ForeignKey("news_page.id"), primary_key=True)
+    file_id: Mapped[int] = mapped_column(ForeignKey("file.id"), primary_key=True)
