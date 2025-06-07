6769f40c26e470454b856b665d461268c7cdfe7c
i-am-ZhVi <unius6ls66@gmail.com>
2025-06-07 15:38:27 +0300

    schema person end

--- backend/src/schemas/person.py
+++ backend/src/schemas/person.py
@@ -22,3 +22,18 @@ class PersonPost(BaseModel):
 
 class PersonGet(PersonPost):
     id: int
+
+
+class PersonRel(PersonGet):
+    posts: list["PostGet"]
+    icon: "FileGet"
+    teacher_groups: List["GroupGet"]
+    strudent_groups: List["GroupGet"]
+    specialties: list["SpecialtiesGet"]
+    substitutions_specialties: list["Substitutions_SpecialtiesGet"]
+    professions: list["ProfessionsGet"]
+    substitutions_professions: list["Substitutions_ProfessionsGet"]
+    chats: list["ChatGet"]
+    channels: list["ChannelGet"]
+    subscribes_on_chats: list["ChatGet"]
+    subscribes_on_channels: list["ChannelGet"]
